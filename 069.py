f9d805e042afa19296739ed2d1cf34a03c8d71c4
bmdyy <45366012+bmdyy@users.noreply.github.com>
2019-07-31 15:01:41 +0300

    Update 069.py

--- 069.py
+++ 069.py
@@ -1,25 +1,24 @@
-def ggT(a, b):
-  i = max(a, b);
-  j = min(a, b);
-  k = 0;
-  l = 1;
-  prev_l = 1;
-
-  while l > 0:
-    k = i // j;
-    prev_l = l;
-    l = i - k * j;
-    # print("%d = %d * %d + %d" % (i, j, k, l));
-    i = j;
-    j = l;
-
-  return prev_l;
-
 def phi(n):
-  ret = [];
-  for i in range(1, n):
-    if ggT(i, n) == 1:
-      ret.append(i);
+  ret = n;
+  i = 2;
+  while i * i <= n:
+    if n % i == 0:
+      while n % i == 0:
+        n /= i;
+      ret = ret - ret / i;
+    i = i + 1;
+  if n > 1:
+    ret = ret - ret / n;
   return ret;
-  
-print(phi(9));
+
+max_phi = 0;
+max_phi_n = 0;
+for n in range(0, 1000000, 210): # 0, 1000 gave 210 so i just put it there and it worked lol
+  curr_phi = phi(n);
+  if curr_phi == 0:
+    continue;
+  curr_phi = n / curr_phi;
+  if curr_phi > max_phi:
+    max_phi = curr_phi;
+    max_phi_n = n;
+print(max_phi_n);
