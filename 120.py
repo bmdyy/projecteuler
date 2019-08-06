7992fb29f0144f04f3bfd23b3f7189baea9cd3ab
bmdyy <45366012+bmdyy@users.noreply.github.com>
2019-08-06 11:21:52 +0300

    Update 120.py

--- 120.py
+++ 120.py
@@ -2,10 +2,10 @@ def r(a, n):
   return ((a-1)**n + (a+1)**n) % a**2;
 
 def r_max(a):
-  ret = 0;
-  for n in range(1, a, 2):
+  ret = 2;
+  for n in range(1, a * 2, 2):
     # even numbers always 2
-    # pattern repeats after max a digits
+    # pattern repeats after max a*2 digits (?)
     r_ = r(a, n);
     if r_ > ret:
       ret = r_;
@@ -13,5 +13,6 @@ def r_max(a):
 
 t = 0;
 for a in range(3, 1001):
+  print(a);
   t += r_max(a);
 print(t);
